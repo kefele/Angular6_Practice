8a20896ba0173726ea9cf73e65e67165178a8a19
Branham <jonathonbranham@nexsystech.com>
2018-11-09 16:31:41 -0500

    end of day

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,10 +1,39 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
-@Injectable()
+@Injectable({
+    providedIn: "root"
+})
+
+   
 export class ProductService{
+    private productUrl = "api/products/products.json"
+
+    constructor(private http: HttpClient){}
     getProducts(): IProduct[]{
         return [
+            {
+                "productId": 8,
+                "productName": "Saw",
+                "productCode": "TBX-0022",
+                "releaseDate": "May 15, 2016",
+                "description": "15-inch steel blade hand saw",
+                "price": 11.55,
+                "starRating": 3.7,
+                "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+              },
+              {
+                "productId": 10,
+                "productName": "Video Game Controller",
+                "productCode": "GMG-0042",
+                "releaseDate": "October 15, 2015",
+                "description": "Standard two-button video game controller",
+                "price": 35.95,
+                "starRating": 4.6,
+                "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+              },
             {
                 "productId": 2,
                 "productName": "Garden Cart",
